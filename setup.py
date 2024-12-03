add46ba4a566232dbf40751c566b3802cbab6166
zbovaird <zbovaird@gmail.com>
2024-12-02 20:49:42 -0500

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uhg",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=['tests*', 'docs*', 'examples*']),
     package_data={
         'uhg': ['*.py'],
